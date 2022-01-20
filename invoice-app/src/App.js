2f1593e9bba49aeb4657850986ecf781aab752ee
rizkaumi <93757521+rizkaumi@users.noreply.github.com>
2022-01-20 11:41:06 +0700

    rizka -add route to invoice form

--- invoice-app/src/App.js
+++ invoice-app/src/App.js
@@ -5,6 +5,7 @@ import Header from "./Layout/Header/Header";
 import Login from "../src/pages/Login/Form"
 import Home from "../src/pages/Home/Home"
 import InvoicePage from "./Layout/Invoice/InvoicePage";
+import InvoiceForm from './Layout/Invoice/InvoiceForm';
 
 import styled from 'styled-components';
 const Container = styled.div`
@@ -25,6 +26,7 @@ function App() {
           <Route exact path={"/login"} element={<Login/>} />
           <Route path={"/dashboard"} element={<Home/>} />
           <Route path={"/invoice"} element={<InvoicePage/>} />
+          <Route path={"/create-invoice"} element={<InvoiceForm/>} />
         </Routes>
       </Container>
       {/* Footer */}

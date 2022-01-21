33470590ed9da9d38f296ba1e8ce6f5300b2a792
Novanna Rahma Zani <novannarahmazani@gmail.com>
2022-01-21 09:51:51 +0700

    chip status

--- invoice-app/src/Components/InvoiceTable/InvoiceTable.js
+++ invoice-app/src/Components/InvoiceTable/InvoiceTable.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import Chip from '@mui/material/Chip';
 import clsx from 'clsx';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
   { field: 'status', headerName: 'Status', width: 100,
-    // cellClassName: (params) => clsx('payment-status', { 
-    //   unpaid: params.value === "unpaid",
-    //   paid: params.value === "paid",
-    //   partial: params.value === "partial",
-    // }),
+    cellClassName: (params) => clsx('payment-status', { 
+      unpaid: params.value === "unpaid",
+      paid: params.value === "paid",
+      partial: params.value === "partial",
+    }),
     renderCell: (params) => {
       // unpaid =  params.value === "unpaid",
       // paid = params.value === "paid",
@@ -20,7 +20,15 @@ const columns = [
         <Chip 
           variant = "outlined"
           label = {params.row.status}
-          color = "primary"
+          // color = {{unpaid} === "unpaid" && "primary" ||
+          // {paid} === "paid" && "success" ||
+          // {partial} === "partial" && "warning"
+          // }
+          sx = {{
+              '{params.row.status} === "unpaid"': { color: "error" },
+              '{params.row.status} === "paid"':  { color:"primary" },
+              '{params.row.status} === "partial"': { color:"success" },
+          }}
         />
       );
     }
@@ -31,46 +39,24 @@ const columns = [
   { field: 'dueDate', headerName: 'Due Date', width: 120, align:'center' },
   { field: 'amount', type:'currency', headerName: 'Amount', width: 120 },
   { field: 'description', headerName: 'Description', width: 150, },
-  { field: 'detailInvoice', headerName: 'Detail', width: 80 },
+  { field: 'detailInvoice', headerName: 'Detail', width: 80},
 ];
 
 const rows = [
-  { id: 1, status: 'unpaid', invoiceNum: 'PSM/01/22/X123', clientName:'Jungkook', invoiceDate: '11/01/2022', dueDate: '03/05/2022', amount: 500000, description: 'belum ada berkas yang dilengkapi', detailInvoice:'button' },
+  { id: 1, status: 'unpaid', invoiceNum: 'PSM/01/22/X123', clientName:'Tiara', invoiceDate: '11/01/2022', dueDate: '03/05/2022', amount: 500000, description: 'belum ada berkas yang dilengkapi', detailInvoice:'button' },
   { id: 2, status: 'paid', invoiceNum: 'PSM/01/22/X123', clientName:'Jungkook', invoiceDate: '11/01/2022', dueDate: '03/05/2022', amount: 500000, description: 'belum ada berkas yang dilengkapi', detailInvoice:'button' }
 ];
 
 export default function InvoiceTable() {
   return (
-    // <div style={{ height: 400, width: '100%' }}>
-      <Box sx={{
-        height: 500,
-        width: '100%',
-         '& .payment-status.unpaid': { border: 'solid 0px transparent', backgroundColor: '#ffaa91', padding: '8px 18px', borderRadius: '0px'},
-         '& .payment-status.partial': { border: 'solid 0px white', backgroundColor: '#baddff', padding: '8px 18px', borderRadius: '0px'},
-         '& .payment-status.paid': { border: 'solid 0px white', backgroundColor: '#a5ffcd', padding: '8px 18px', borderRadius: '0px'}
-      }}>
-        {/* <Chip 
-          variant = "outlined"
-          label = {rows.status}
-          sx={{
-          '& .payment-status.unpaid': { color:"primary" },
-          '& .payment-status.partial':  { color:"primary" },
-          '& .payment-status.paid': { color:"primary" },
-        }}
-        /> */}
+    <div style={{ height: 400, width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
-        // sx={{
-        //   '& .Unpaid': { border: 'solid 0px red', backgroundColor: '#ffaa91', padding: '8px 18px', borderRadius: '20px'},
-        //   '& .Partial': { border: 'solid 0px #1976d2', backgroundColor: '#baddff', padding: '8px 18px', borderRadius: '20px'},
-        //   '& .Paid': {border: 'solid 0px green', backgroundColor: '#a5ffcd', padding: '8px 18px', borderRadius: '20px'}
-        // }}
       />
-      </Box>
-    // </div>
+    </div>
   );
 }

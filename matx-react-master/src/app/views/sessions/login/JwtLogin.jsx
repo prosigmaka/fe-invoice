d4b89a19ce1a55f0ae7928aa876401e329b4feb5
Imam Hilmi <imam.hilmi253@gmail.com>
2022-01-24 01:16:32 +0700

    Add lottie files depedency & changes IMG at Login

--- matx-react-master/src/app/views/sessions/login/JwtLogin.jsx
+++ matx-react-master/src/app/views/sessions/login/JwtLogin.jsx
@@ -12,6 +12,7 @@ import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator'
 import { Paragraph } from 'app/components/Typography'
 import "./JwtLogin.css"
 import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai'
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const FlexBox = styled(Box)(() => ({
     display: 'flex',
@@ -93,10 +94,16 @@ const JwtLogin = () => {
                 <Grid container>
                     <Grid item lg={5} md={5} sm={5} xs={12}>
                         <JustifyBox p={4} height="100%">
-                            <IMG
+                            {/* <IMG
                                 src="/assets/images/illustrations/dreamer.svg"
                                 alt=""
-                            />
+                            /> */}
+                            <Player
+                                autoplay
+                                loop
+                                src="https://assets4.lottiefiles.com/packages/lf20_t4m6huub.json"
+                                style={{height:'200px', width: '300px' }}
+                                />
                         </JustifyBox>
                     </Grid>
                     <Grid item lg={7} md={7} sm={7} xs={12}>
